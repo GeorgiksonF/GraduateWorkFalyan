715eb0b9a4221283cdfb3d5e72e3ac0fc84b3b58
Falyan.G <Falyan.G@digdes.com>
2022-01-17 17:47:21 +0400

    fix: publication page

--- front/src/views/Publication.vue
+++ front/src/views/Publication.vue
@@ -1,17 +1,57 @@
 <template>
     <div class="container">
-        <div class="article">
-            <h1 class="article__title">{{articleInfo.title}}</h1>
+        <div class="text-center spinner" v-if="preloader">
+            <b-spinner label="Large Spinner"></b-spinner>
+        </div>
+        <div class="article" v-else>
+            <h1 class="article__title">
+                {{articleInfo.title}}
+                <b-badge variant="dark">Rating: {{articleInfo.rating}}</b-badge>
+            </h1>
             <p class="article__abstract">{{articleInfo.abstract}}</p>
-            
-            <ul class="article__authors">
-                <li 
-                    class="article__author"
-                    v-for="item in articleInfo.authors"
-                    :key="item._id">
-                    <b-link :to="getUri(item._id)">{{item.author}}</b-link>
-                </li>
-            </ul>
+            <p class="article__journal">Journal: <span class="text-primary">{{articleInfo.journal.title}}</span></p>
+            <b-row>
+                <b-col>
+                    <h2 class="article__authors-list">Authors list</h2>
+                    <ul class="article__authors">
+                        <li 
+                            class="article__author"
+                            v-for="item in articleInfo.authors"
+                            :key="item._id">
+                            <b-link :to="getUri(item._id)">{{item.author}}</b-link>
+                        </li>
+                    </ul>
+                </b-col>
+                <b-col>
+                    <div class="article__info">
+                        <h2 class="article__info-title">Article info</h2>
+                        <table class="table article__table">
+                            <tbody>
+                                <tr>
+                                    <th>Rating</th>
+                                    <td>{{articleInfo.rating || null}}</td>
+                                </tr>
+                                <tr>
+                                    <th>Published</th>
+                                    <td>{{articleInfo.published || '-'}}</td>
+                                </tr>
+                                <tr>
+                                    <th>DOI</th>
+                                    <td>{{articleInfo.doi || '-'}}</td>
+                                </tr>
+                                <tr>
+                                    <th>ISSUE</th>
+                                    <td>{{articleInfo.issue || null}}</td>
+                                </tr>
+                                <tr>
+                                    <th>Valume</th>
+                                    <td>{{articleInfo.volume || '-'}}</td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                </b-col>
+            </b-row>
         </div>
     </div>
 </template>
@@ -21,7 +61,10 @@ import { publicationsApi } from '../api/api'
 export default {
     data() {
         return {
-            articleInfo: {}
+            articleInfo: {
+                journal: {}
+            },
+            preloader: false
         }
     },
     methods: {
@@ -29,6 +72,7 @@ export default {
             return `/authors/id=${id}`
         },
         fetchArticle() {
+            this.preloader = true
             const dto = {
                 articleId: this.$router.currentRoute.params.id
             }
@@ -36,6 +80,7 @@ export default {
             return publicationsApi.getArticleBuId(dto)
                     .then(res => {
                         this.articleInfo = res.data.article || {}
+                        this.preloader = false
                     })
         }
     },
@@ -44,3 +89,46 @@ export default {
     }
 }
 </script>
+
+<style lang="scss">
+    .spinner {
+        margin-top: 100px;
+    }
+
+    .article {
+        &__title {
+            margin-top: 40px;
+        }
+
+        &__abstract {
+            margin-top: 15px;
+            font-size: 20px;
+        }
+        
+        &__journal {
+            font-size: 20px;
+        }
+
+        &__authors-list {
+            margin-top: 20px;
+        }
+
+        &__authors {
+            margin-top: 15px;
+        }
+
+        &__author {
+            font-size: 20px;
+        }
+
+        &__info {
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+        }
+
+        &__table {
+            font-size: 20px;
+        }
+    }
+</style>
