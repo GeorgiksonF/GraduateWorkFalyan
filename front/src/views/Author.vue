ac1875994cb14869f4afd8d750a8859fea768ff3
Falyan.G <Falyan.G@digdes.com>
2022-01-17 18:01:40 +0400

    fix: author page

--- front/src/views/Author.vue
+++ front/src/views/Author.vue
@@ -1,19 +1,23 @@
 <template>
     <div class="container">
-        <div class="author">
-            <h1 class="author__name">{{authorInfo.author}}</h1>
-            <p class="author__info">
-                Рейтинг данного автора:
-                <span class="author_rating">{{authorInfo.rating}}</span>
-            </p>
-            <h2>Публикации автора:</h2>
-            <ul class="author__articles">
-                <li v-for="article in authorInfo.articles" :key="article.id">
-                    <b-link :to="getUri(article._id)">
-                        {{article.title}}
-                    </b-link>
-                </li>
-            </ul>
+        <div class="text-center spinner" v-if="preloader">
+            <b-spinner label="Large Spinner"></b-spinner>
+        </div>
+        <div class="author" v-else>
+            <h1 class="author__name">
+                {{authorInfo.author}}
+                <b-badge variant="dark">Rating: {{authorInfo.rating}}</b-badge>
+            </h1>
+            <div class="author__publications">
+                <h2>Публикации автора:</h2>
+                <ul class="author__articles">
+                    <li v-for="article in authorInfo.articles" :key="article.id">
+                        <b-link :to="getUri(article._id)">
+                            {{article.title}}
+                        </b-link>
+                    </li>
+                </ul>
+            </div>
         </div>
     </div>
 </template>
@@ -23,7 +27,8 @@ import { authorsApi } from '../api/api'
 export default {
     data() {
         return {
-            authorInfo: {}
+            authorInfo: {},
+            preloader: false
         }
     },
     methods: {
@@ -31,6 +36,7 @@ export default {
             return `/publications/id=${id}`
         },
         fetchAuthor() {
+            this.preloader = true
             const dto = {
                 authorId: this.$router.currentRoute.params.id
             }
@@ -38,6 +44,7 @@ export default {
             return authorsApi.getAuthorById(dto)
                     .then(res => {
                         this.authorInfo = res.data.author || {}
+                        this.preloader = false
                     })
         }
     },
@@ -46,3 +53,22 @@ export default {
     }
 }
 </script>
+
+<style lang="scss">
+    .spinner {
+        margin-top: 100px;
+    }
+    .author {
+        &__name {
+            margin-top: 50px;
+        }
+
+        &__publications {
+            margin-top: 40px;
+        }
+
+        &__articles {
+            font-size: 23px;
+        }
+    }
+</style>
